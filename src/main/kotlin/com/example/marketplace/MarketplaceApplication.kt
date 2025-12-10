ad5f46cb9ee396b6775b32db039a328a3b484a83
Praveen Kumar S <38393882+praveenkmrs@users.noreply.github.com>
2025-12-10 12:29:32 +0530

    feat: Add src/main/kotlin/com/example/marketplace/MarketplaceApplication.kt

---
+++ src/main/kotlin/com/example/marketplace/MarketplaceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.marketplace
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MarketplaceApplication
+
+fun main(args: Array<String>) {
+    runApplication<MarketplaceApplication>(*args)
+}
