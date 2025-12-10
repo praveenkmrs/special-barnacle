8010e1838a4373fc4ad466da238a5cadc08624a2
Praveen Kumar S <38393882+praveenkmrs@users.noreply.github.com>
2025-12-10 12:29:33 +0530

    feat: Add src/test/kotlin/com/example/marketplace/MarketplaceApplicationTests.kt

---
+++ src/test/kotlin/com/example/marketplace/MarketplaceApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.example.marketplace
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MarketplaceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
